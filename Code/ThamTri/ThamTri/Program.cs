0ac1a6c0979cc8fccc289f239874da9e0ecd2228
nguyentuankhiem <tuankhiem.maicogroup@gmail.com>
2021-06-16 16:14:00 +0700

    end

--- Code/ThamTri/ThamTri/Program.cs
+++ Code/ThamTri/ThamTri/Program.cs
@@ -6,7 +6,21 @@ namespace ThamTri
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int num = 5;
+            Console.WriteLine($"gia tri ban dau: {num}");
+            // su dung phuong thuc
+            TangSo(num);
+            Console.WriteLine($"gia tri sau khi dung phuong thuc: {num}");
+            Console.ReadLine();
+        }
+
+        // method
+        static void TangSo(int num)
+        {
+            num++;
+            // parameter se duoc ra mot ban copy va khi ket thuc phuong thuc 
+            // gia tri copy do se bien mat, gia tri goc khong thay doi
+
         }
     }
 }
