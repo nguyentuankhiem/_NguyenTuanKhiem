7e2303467fe628c7e4866303ac16f0a0bac2719b
nguyentuankhiem <tuankhiem.maicogroup@gmail.com>
2021-06-16 16:18:04 +0700

    end

--- Code/Out/Out/Program.cs
+++ Code/Out/Out/Program.cs
@@ -6,7 +6,22 @@ namespace Out
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int num = 5;
+            Console.WriteLine($"gia tri ban dau: {num}");
+            // su dung phuong thuc
+            TangSo(out num); // gia tri ban dau khi chua co out la 5
+            Console.WriteLine($"gia tri sau khi dung phuong thuc: {num}"); // gia tri sau khi dung phuong thuc la 2
+            Console.ReadLine();
+        }
+
+        // method
+        static void TangSo(out int num)
+        {
+            num = 1;
+            num++;
+            // khi su dung phuong thuc co chua out thi ta khong can phai khai bao gia tri ban dau
+            // tham chieu bang out giong nhu mot noi chua ket qua tra ve
+
         }
     }
 }
