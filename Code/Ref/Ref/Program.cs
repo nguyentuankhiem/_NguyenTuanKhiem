a3841a5bf518b915f85b732ca79469d3726e8db6
nguyentuankhiem <tuankhiem.maicogroup@gmail.com>
2021-06-16 16:09:36 +0700

    end

--- Code/Ref/Ref/Program.cs
+++ Code/Ref/Ref/Program.cs
@@ -6,8 +6,20 @@ namespace Ref
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int num = 5;
+            Console.WriteLine($"gia tri ban dau: {num}");
+            // su dung phuong thuc
+            TangSo(ref num); // luu y khi goi phuong thuc thi parameter cung phai co ref
+            Console.WriteLine($"gia tri sau khi dung phuong thuc: {num}");
+            Console.ReadLine();
+        }
 
+        // method
+        static void TangSo(ref int num)
+        {
+            num++;
+            // parameter se duoc su dung truc tiep chu khong 
+            
         }
     }
 }
