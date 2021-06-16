7e8d0e8724244942b311f03865711fca59792e1e
nguyentuankhiem <tuankhiem.maicogroup@gmail.com>
2021-06-16 16:02:10 +0700

    start

---
+++ Code/Ref/Ref/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Ref
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
