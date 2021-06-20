c0e5da93e613f4c575c160fe5586ef683d41415b
kebxkhw <tuankhiem2405@gmail.com>
2021-06-20 14:17:54 +0700

    start

---
+++ C#NangCao/SameLinQMethod/SameLinQMethod/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SameLinQMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
