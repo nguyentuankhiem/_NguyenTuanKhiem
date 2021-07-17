3584e32bc9158b9ce460a76c5604dfc560eeece3
kebxkhw <tuankhiem2405@gmail.com>
2021-07-17 13:33:03 +0700

    sua lai code theo y anh Thong - lan 2

--- oop/oop/Devices/DevicesClass.cs
+++ oop/oop/Devices/DevicesClass.cs
@@ -6,26 +6,26 @@ namespace oop.Devices
 {
     abstract class DevicesClass
     {
-        protected string ID { get; set; }
-        protected string Name { get; set; }
-        protected string Producer { get; set; }
-        protected int Amount { get; set; }
-        protected int Price { get; set; }
+        protected string id { get; set; }
+        protected string name { get; set; }
+        protected string producer { get; set; }
+        protected int amount { get; set; }
+        protected int price { get; set; }
 
         public virtual void AddDevice()
         {
             Console.Write("\n\t\t\t\tMã sản phẩm: ");
-            this.ID = Console.ReadLine();
+            this.id = Console.ReadLine();
             Console.Write("\n\t\t\t\tTên sản phẩm: ");
-            this.Name = Console.ReadLine();
+            this.name = Console.ReadLine();
             Console.Write("\n\t\t\t\tNơi sản xuất: ");
-            this.Producer = Console.ReadLine();
+            this.producer = Console.ReadLine();
             Console.Write("\n\t\t\t\tSố lượng: ");
             while (true)
             {
                 try
                 {
-                    this.Amount = int.Parse(Console.ReadLine());
+                    this.amount = int.Parse(Console.ReadLine());
                     break;
                 }
                 catch (Exception)
